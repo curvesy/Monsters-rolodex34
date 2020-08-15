5bbc979ce07f56cbf5aef5d568a3f776ad197545
git@github.com:curvesy <sinakorhanishirazi3494@gmail.com>
2020-08-14 17:52:13 -0700

    Updates

---
+++ precache-manifest.7565a35cd54360955d27dd98f0c009ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b38efa7735d4c0309cd3cdb6cc2a914",
+    "url": "/curvesy/Monsters-rolodex34.git/index.html"
+  },
+  {
+    "revision": "539e00947deb9ab7cca4",
+    "url": "/curvesy/Monsters-rolodex34.git/static/css/main.cc1ebeb0.chunk.css"
+  },
+  {
+    "revision": "e901eccedc4a79822f85",
+    "url": "/curvesy/Monsters-rolodex34.git/static/js/2.390247ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/curvesy/Monsters-rolodex34.git/static/js/2.390247ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "539e00947deb9ab7cca4",
+    "url": "/curvesy/Monsters-rolodex34.git/static/js/main.ea094c5e.chunk.js"
+  },
+  {
+    "revision": "3f61bfc36279fa0d5dc9",
+    "url": "/curvesy/Monsters-rolodex34.git/static/js/runtime-main.795a73de.js"
+  }
+]);
